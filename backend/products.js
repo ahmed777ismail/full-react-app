5488445913e8a9683f01cfdf5ea54d90320efa56
ahmed777ismail <ahmed1kaballo@gmail.com>
2024-01-24 16:53:13 +0200

    Creating a Node.js API

---
+++ backend/products.js
@@ -0,0 +1,39 @@
+const products = [
+  {
+    id: 1,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 100,
+    imageLink:
+      "https://res.cloudinary.com/dwllquzyu/image/upload/v1706107805/online%20store/1_opas2z.jpg",
+  },
+  {
+    id: 2,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 200,
+    imageLink:
+      "https://res.cloudinary.com/dwllquzyu/image/upload/v1706107805/online%20store/3_g6lj5z.jpg",
+  },
+  {
+    id: 3,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 300,
+    imageLink:
+      "https://res.cloudinary.com/dwllquzyu/image/upload/v1706107805/online%20store/4_e1k82o.jpg",
+  },
+  {
+    id: 4,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 400,
+    imageLink:
+      "https://res.cloudinary.com/dwllquzyu/image/upload/v1706107804/online%20store/2_srb0eh.jpg",
+  },
+];
+module.exports = products;
